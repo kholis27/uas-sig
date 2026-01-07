91879031b8a34af7a52b3beb9e3ef17c9baa29fd
kholis27 <knur46446@gmail.com>
2026-01-07 11:25:10 +0700

    Add files via upload

---
+++ qgis2web_2025_12_23-00_00_34_486417/styles/LapakGIS_Batas_Kabupaten_2024_1_style.js
@@ -0,0 +1,54 @@
+var size = 0;
+var placement = 'point';
+
+var style_LapakGIS_Batas_Kabupaten_2024_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("jml pr");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("kabupaten") !== null) {
+        labelText = String(feature.get("kabupaten"));
+    }
+    if (value >= 2.000000 && value <= 14.500000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 14.500000 && value <= 21.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,170,170,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 21.000000 && value <= 29.500000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,85,85,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 29.500000 && value <= 54.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
---
+++ qgis2web_2025_12_23-00_15_22_897801/layers/layers.js
@@ -0,0 +1,41 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_LapakGIS_Batas_Kabupaten_2024_1 = new ol.format.GeoJSON();
+var features_LapakGIS_Batas_Kabupaten_2024_1 = format_LapakGIS_Batas_Kabupaten_2024_1.readFeatures(json_LapakGIS_Batas_Kabupaten_2024_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_LapakGIS_Batas_Kabupaten_2024_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_LapakGIS_Batas_Kabupaten_2024_1.addFeatures(features_LapakGIS_Batas_Kabupaten_2024_1);
+var lyr_LapakGIS_Batas_Kabupaten_2024_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_LapakGIS_Batas_Kabupaten_2024_1, 
+                style: style_LapakGIS_Batas_Kabupaten_2024_1,
+                popuplayertitle: 'LapakGIS_Batas_Kabupaten_2024',
+                interactive: true,
+    title: 'LapakGIS_Batas_Kabupaten_2024<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_0.png" /> 2878 - 7141<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_1.png" /> 7141 - 9114<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_2.png" /> 9114 - 11262<br />\
+    <img src="styles/legend/LapakGIS_Batas_Kabupaten_2024_1_3.png" /> 11262 - 19719<br />' });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_LapakGIS_Batas_Kabupaten_2024_1.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_LapakGIS_Batas_Kabupaten_2024_1];
+lyr_LapakGIS_Batas_Kabupaten_2024_1.set('fieldAliases', {'kabupaten': 'kabupaten', 'provinsi': 'provinsi', 'swalayan': 'swalayan', 'jml pr': 'jml pr', 'jml ppr': 'jml ppr', });
+lyr_LapakGIS_Batas_Kabupaten_2024_1.set('fieldImages', {'kabupaten': 'TextEdit', 'provinsi': 'TextEdit', 'swalayan': 'TextEdit', 'jml pr': 'Range', 'jml ppr': 'Range', });
+lyr_LapakGIS_Batas_Kabupaten_2024_1.set('fieldLabels', {'kabupaten': 'inline label - always visible', 'provinsi': 'inline label - always visible', 'swalayan': 'inline label - always visible', 'jml pr': 'inline label - always visible', 'jml ppr': 'inline label - always visible', });
+lyr_LapakGIS_Batas_Kabupaten_2024_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
